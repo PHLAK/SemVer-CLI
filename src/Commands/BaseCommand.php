9d7275771c693f1f6089d87feee85dc51c10d1b6
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2016-06-25 13:16:55 -0700

    Added docblocks to BaseCommand class methods

--- src/Commands/BaseCommand.php
+++ src/Commands/BaseCommand.php
@@ -12,6 +12,11 @@ abstract class BaseCommand extends Command {
     /** @var array Semantic version data file */
     protected $semverFile = '.semver';
 
+    /**
+     * Get the SemVer object from the data file
+     *
+     * @return SemVer SemVer object
+     */
     protected function readVersionFromDisk() {
         if (! $contents = @file_get_contents($this->semverFile)) {
             throw new RuntimeException('Semantic versioning not intialized in this directory');
@@ -19,6 +24,13 @@ abstract class BaseCommand extends Command {
         return unserialize($contents);
     }
 
+    /**
+     * Write a SemVer object to the data file
+     *
+     * @param  SemVer $semver A SemVer object
+     *
+     * @return boolean        True on success
+     */
     protected function writeVersionToDisk(SemVer $semver) {
         if (file_put_contents($this->semverFile, serialize($semver), LOCK_EX) === false) {
             throw new RuntimeException('Failed to write data to disk');
@@ -26,6 +38,14 @@ abstract class BaseCommand extends Command {
         return true;
     }
 
+    /**
+     * Construct the method name for a given prefix + property
+     *
+     * @param  string $prefix   Method prefix
+     * @param  string $property Property value
+     *
+     * @return string           Method name
+     */
     protected function getMethodFromProperty($prefix, $property) {
         $semver = $this->readVersionFromDisk();
         $method = $prefix . implode('', array_map('ucfirst', explode('-', $property)));
