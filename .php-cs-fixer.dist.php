227df50f74226f36f955fa6b4136c288e85cd054
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2021-11-01 21:33:12 -0700

    Updated phlak/coding-standards to 2.X

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -6,7 +6,7 @@ $finder = PhpCsFixer\Finder::create()->in([
     __DIR__ . DIRECTORY_SEPARATOR . 'bin',
     __DIR__ . DIRECTORY_SEPARATOR . 'src',
     __DIR__ . DIRECTORY_SEPARATOR . 'tests',
-])->notPath(['cache', 'vendor']);
+]);
 
 return PHLAK\CodingStandards\ConfigFactory::make($finder, [
     'php_unit_set_up_tear_down_visibility' => true,
